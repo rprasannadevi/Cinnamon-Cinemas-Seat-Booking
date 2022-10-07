ba22edbf47adc6db3b04c01359f8469fbf99bcd8
Prasanna <prasannadevi@gmail.com>
2022-10-07 01:09:27 +0100

    Added Code to book the tickets based on random number generation.

--- CinnamonCinemasSeatBooking.Tests/CinnamonCinemasSeatBookingTests.cs
+++ CinnamonCinemasSeatBooking.Tests/CinnamonCinemasSeatBookingTests.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using CinnamonCinemasSeatBooking.Models;
 using FluentAssertions;
 
@@ -7,27 +8,49 @@ public class CinnamonCinemasSeatBookingTests
 {
     private MovieTheatre _MovieTheatre;
     private BookingManager _BookingManager;
+    public Hashtable? AvailableSeats;
 
     [SetUp]
     public void Setup()
     {
-        _MovieTheatre = new MovieTheatre();
+        var noOfrows = 3;
+        var noOfSeatsInARow = 5;
+        _MovieTheatre = new MovieTheatre(noOfrows, noOfSeatsInARow);
         _BookingManager = new BookingManager();
+        _BookingManager.SetCapacity(noOfrows, noOfSeatsInARow);
+        _BookingManager.AvailableSeatsInformation();
     }
 
     [Test]
     public void Get_the_Capacity_Of_Movie_Theater()
     {
-        _MovieTheatre.SetCapacity(3, 5);
         _MovieTheatre.TotalCapacity.Should().Be(15);
-        //AvailableSeatsInfo - May be Hashtable with (Row, NoOfSeatsinthisRowAvailable)
+    }
+
+    [Test]
+    public void Print_The_Available_Seats()
+    {
+        _BookingManager.AvailableSeatsInformation();
+        Console.WriteLine(" Avaiable Seats are: ");
+        foreach (string Key in BookingManager.AvailableSeatsInfo.Keys)
+            Console.WriteLine(String.Format("{0} : {1}", Key, BookingManager.AvailableSeatsInfo[Key]));
     }
 
     [Test]
     public void Book_Seats_According_To_Random_Number()
     {
         Random randomNumber = new Random();
-        randomNumber.Next(1,4);
-        _BookingManager.BookSeats(2).Should().Be(true);
+        int randNumber;
+        bool isSeatsAvailable = true;
+        while(isSeatsAvailable)
+        {
+            randNumber = randomNumber.Next(1, 4);
+            isSeatsAvailable = _BookingManager.BookTickets(randNumber);
+            if (isSeatsAvailable == false)
+                break;
+        }
+        _BookingManager.AvailableSeatsInformation();
+        foreach (string Key in BookingManager.AvailableSeatsInfo.Keys)
+            Console.WriteLine(String.Format("{0} : {1}", Key, BookingManager.AvailableSeatsInfo[Key]));
     }
 }
